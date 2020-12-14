1e333c3f7c65d2df985175f12ffebdb3d95e245b
nh35813219254@gmail.com <ec2-user@ip-172-31-20-226.ec2.internal>
2020-12-14 05:45:20 +0000

    [Caravan]add2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'blogs/index'
+  get 'blogs/show'
+  get 'blogs/new'
+  get 'blogs/edit'
+  get 'blogs/index'
+
+  get 'blogs/show'
+
+  get 'blogs/new'
+
+  get 'blogs/edit'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :blogs
 end
